5b2ba970f2df47a5271930236f07627e2c4e9797
Usman Musa <usmanmusa1920@gmail.com>
2022-12-26 17:28:29 +0100

    refactor __init__.py

--- kyaah/__init__.py
+++ kyaah/__init__.py
@@ -1,46 +1,3 @@
-# -*- coding: utf-8 -*-
-
-#   __
-#  /__)  _  _     _   _ _/   _
-# / (   (- (/ (/ (- _)  /  _)
-#          /
-class mapp:
-    pass
-"""
-Requests HTTP Library
-~~~~~~~~~~~~~~~~~~~~~
-
-Requests is an HTTP library, written in Python, for human beings.
-Basic GET usage:
-
-   >>> import requests
-   >>> r = requests.get('https://www.python.org')
-   >>> r.status_code
-   200
-   >>> b'Python is a programming language' in r.content
-   True
-
-... or POST:
-
-   >>> payload = dict(key1='value1', key2='value2')
-   >>> r = requests.post('https://httpbin.org/post', data=payload)
-   >>> print(r.text)
-   {
-     ...
-     "form": {
-       "key1": "value1",
-       "key2": "value2"
-     },
-     ...
-   }
-
-The other HTTP methods are supported - see `requests.api`. Full documentation
-is at <https://requests.readthedocs.io>.
-
-:copyright: (c) 2017 by Kenneth Reitz.
-:license: Apache 2.0, see LICENSE for more details.
-"""
-
 
 """
         #    #   #   #    ##      ##    #    #
@@ -50,7 +7,24 @@ is at <https://requests.readthedocs.io>.
         #   #      #    #    #  #    #  #    #
         #    #     #    #    #  #    #  #    #
 
+        Kyaah abstract away SMTP mail operations
+
         Simplify your email message with `kyaah`
+
+        Usage at a glance:
+        
+            >>> import kyaah
+            
+            >>>    sender = "my_email@gmail.com"
+            >>>    receiver = ["receiver_1@gmail.com", "receiver_2@gmail.com"]
+            >>>    passwd = "*********"
+
+            >>>    server = "gmail"
+            >>>    subj = f"Hellow world!"
+            >>>    body = "Lorem ipsum dolor sit amet adipisicing elit, rerum voluptate ipsum volupt."
+
+            >>> kyaah.sendMail(
+                    from_usr=sender, to_usr=receiver, svr=server, subject=subj, body=body, mail_passwd=passwd)
 """
 
 __title__ = "kyaah"

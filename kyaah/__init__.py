562926b3f8bc36837f1b8138e36122c02398bcb0
Usman Musa <usmanmusa1920@gmail.com>
2023-02-20 02:05:36 +0100

    update author info in the package __init__.py file

--- kyaah/__init__.py
+++ kyaah/__init__.py
@@ -29,10 +29,13 @@
 
 
 __title__ = "kyaah"
+__version__ = "0.0.3"
 __author__ = "Usman Musa"
 __author_email__ = "usmanmusa1920@gmail.com"
 __author_website__ = "https://usmanmusa1920.github.io"
-__copyright__ = "Copyright 2022 Usman Musa"
+__repository__ = "https://github.com/usmanmusa1920/sakyum"
+__website__ = "https://kyaah.readthedocs.io/en/latest/"
+__copyright__ = "Copyright (C) 2022 - 2023 Usman Musa"
 
 
 status = {

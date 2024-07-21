0c4312aac1f074a0014ef5cc80dcc4cf87b67e2b
Usman Musa <usmanmusa1920@gmail.com>
2024-07-21 13:11:31 +0100

    Update CHANGELOG version of release (mistake of previous release v0.1.14 i.e `(*/august/2024)` to `(21/july/2024)`)

--- kyaah/__init__.py
+++ kyaah/__init__.py
@@ -40,7 +40,7 @@
 from datetime import datetime
 
 __title__ = 'kyaah'
-__version__ = '0.1.14'
+__version__ = '0.1.15'
 __author__ = 'Usman Musa'
 __author_email__ = 'usmanmusa1920@gmail.com'
 __author_website__ = 'https://usmanmusa1920.github.io'
